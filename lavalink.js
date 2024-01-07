3309c92f5b2eca49042eafb08f4dfb6b1c901117
alvalenda <banysan@msn.com>
2024-01-07 19:20:57 -0300

    Fix lavaLinkSecure boolean parsing

--- lavalink.js
+++ lavalink.js
@@ -8,7 +8,7 @@ const lavaLinkList = lavaLinkString.split('\n').slice(1, -1)
 const lavaLinkHost = lavaLinkList[0].split(' : ')[1].replace(/"/g, '') + ''
 const lavaLinkPort = +lavaLinkList[1].split(' : ')[1]
 const lavaLinkPassword = lavaLinkList[2].split(' : ')[1].replace(/"/g, '') + ''
-const lavaLinkSecure = !!lavaLinkList[3].split(' : ')[1]
+const lavaLinkSecure = lavaLinkList[3].split(' : ')[1].trim() === 'true'
 
 console.log('configurando lavalink...')
 console.log('lavalink host:', lavaLinkHost)
