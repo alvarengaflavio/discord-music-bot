1e61caf69abfda49bf6742e2c59364776894f74a
alvalenda <banysan@msn.com>
2024-01-07 19:23:59 -0300

    Refactor LavaLink console logs

--- lavalink.js
+++ lavalink.js
@@ -10,11 +10,11 @@ const lavaLinkPort = +lavaLinkList[1].split(' : ')[1]
 const lavaLinkPassword = lavaLinkList[2].split(' : ')[1].replace(/"/g, '') + ''
 const lavaLinkSecure = lavaLinkList[3].split(' : ')[1].trim() === 'true'
 
-console.log('configurando lavalink...')
-console.log('lavalink host:', lavaLinkHost)
-console.log('lavalink port:', lavaLinkPort)
-console.log('lavalink password:', lavaLinkPassword)
-console.log('lavalink secure:', lavaLinkSecure)
+console.log(`LavaLink:`,)
+console.log('host:', lavaLinkHost)
+console.log('port:', lavaLinkPort)
+console.log('password:', lavaLinkPassword)
+console.log('secure:', lavaLinkSecure)
 console.log('lavalink configurado!')
 
 module.exports = {
